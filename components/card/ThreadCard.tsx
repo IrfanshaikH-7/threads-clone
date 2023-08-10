119f5c43c6c337ce8838f43a899f11ac901f888e
Irfan shaik <414irfann@gmail.com>
2023-08-10 16:08:46 +0530

    addded profile route and created profile page

--- components/card/ThreadCard.tsx
+++ components/card/ThreadCard.tsx
@@ -64,7 +64,7 @@ const ThreadCard = ({
                         <p className="mt-2 text-small-regular text-light-2">
                             {content}
                         </p>
-                        <div className="mt-5 flex-col gap-3">
+                        <div className={` ${isComment && 'mb-10'} mt-5 flex-col gap-3`}>
                             <div className="flex gap-3.5">
                                 <Image 
                                  src='/assets/heart-gray.svg'
